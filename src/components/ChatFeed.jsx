f7c9e6ff4af48ce60c6a49e0ddd26af6c7f6bd12
wumbles <77227394+wumbles@users.noreply.github.com>
2023-07-04 02:50:10 -0600

    UPDATE: working on chat image-upload feature

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -18,7 +18,14 @@ const ChatFeed = (props) => {
       return (
         <div key={`msg_${index}`} style={{ width: "100%" }}>
           <div className="message-block">
-            {isMyMessage ? <MyMessage /> : <TheirMessage />}
+            {isMyMessage ? (
+              <MyMessage message={message} />
+            ) : (
+              <TheirMessage
+                message={message}
+                lastMessage={messages[lastMessageKey]}
+              />
+            )}
           </div>
           <div
             className="read-receipts"
